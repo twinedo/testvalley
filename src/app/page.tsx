361f0ccf9a254235bc66984a090497b638235dd8
Twin Edo <twin.e@wistkey.dev>
2024-02-14 04:17:23 +0700

    add banners with content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/banner';
 import Toolbar from '@/components/toolbar';
 import Image from 'next/image';
 
@@ -5,7 +6,8 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between bg-white'>
 			<Toolbar />
-			<div className='max-w-[960px] w-full items-center justify-between font-mono text-sm lg:flex'>
+			<div className='max-w-[960px] flex flex-col w-full items-center font-mono text-sm lg:flex'>
+				<Banner />
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Get started by editing&nbsp;
 					<code className='font-mono font-bold'>src/app/page.tsx</code>
--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,9 +1,79 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa6';
+
+const SwiperButtonNext = () => {
+	const swiper = useSwiper();
+	return (
+		<button
+			className='w-11 h-11 p-[10px] bg-[#33333380] flex justify-center items-center rounded-full absolute text-white top-[140px] right-3 z-10'
+			onClick={() => swiper.slideNext()}>
+			<FaChevronRight color='white' size={24} />
+		</button>
+	);
+};
+
+const SwiperButtonPrevious = () => {
+	const swiper = useSwiper();
+	return (
+		<button
+			className='w-11 h-11 p-[10px] bg-[#33333380] flex justify-center items-center rounded-full absolute text-white top-[140px] left-3 z-10'
+			onClick={() => swiper.slidePrev()}>
+			<FaChevronLeft color='white' size={24} />
+		</button>
+	);
+};
 
 function Banner() {
+	const pagination = {
+		clickable: true,
+		renderBullet: function (_: any, className: string) {
+			return (
+				'<span class="' +
+				className +
+				'" style="background-color: white;"></span>'
+			);
+		},
+	};
+
+	const [banners, setBanners] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.testvalley.kr/main-banner/all').then(async (res) => {
+			const response = await res.json();
+			setBanners(response);
+
+			console.log('res', await res.json());
+		});
+	}, []);
+
 	return (
-		<div>
-			<div>Banner</div>
+		<div className='relative z-[2px] w-full border h-[320px]'>
+			<Swiper
+				className='h-[320px] flex justify-center items-center bg-blue'
+				modules={[Pagination]}
+				pagination={pagination}
+				scrollbar={{ draggable: true }}
+				spaceBetween={50}
+				loop
+				slidesPerView={1}
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={(swiper) => console.log(swiper)}>
+				<SwiperButtonNext />
+				{banners.map((o) => (
+					<div key={o?.mainBannerId?.toString()}>
+						<SwiperSlide className='flex justify-center items-center'>
+							<img src={o?.pcImageUrl} className='w-full h-full' />
+						</SwiperSlide>
+					</div>
+				))}
+
+				<SwiperButtonPrevious />
+			</Swiper>
 		</div>
 	);
 }
