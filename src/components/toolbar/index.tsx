5ba90ab20679f4732ee8896362c7e5f70fdd1fa2
Twin Edo <twin.e@wistkey.dev>
2024-02-16 12:02:28 +0700

    restyling toolbar

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -6,22 +6,25 @@ function Toolbar() {
 	return (
 		<div className='sticky top-0 w-full h-[55px] md:h-[72px] bg-white z-10 border-b border-solid border-gray-300    '>
 			<div className='flex relative items-center justify-between max-w-[960px] h-full my-0 mx-auto'>
-				<div className='flex ml-5 md:ml-0 w-full items-center justify-between gap-4'>
-					<Image
-						src={'https://www.testvalley.kr/logo/logo-new.svg'}
-						priority
-						width={128}
-						height={25}
-						alt='logo'
-					/>
-					<div className='hidden md:flex row gap-1 text-[#00D094] text-[16px]'>
+				<div className='flex ml-5 md:ml-0 items-center justify-between md:justify-normal gap-4'>
+					<div className='flex flex-row items-center'>
 						<Image
-							src='https://www.testvalley.kr/common/icon-category.svg'
-							width={16}
-							height={16}
-							alt='category'
+							src={'https://www.testvalley.kr/logo/logo-new.svg'}
+							priority
+							width={128}
+							height={25}
+							alt='logo'
+							className='mr-4'
 						/>
-						카테고리
+						<div className='hidden md:flex row gap-1 text-[#00D094] text-[16px]'>
+							<Image
+								src='https://www.testvalley.kr/common/icon-category.svg'
+								width={16}
+								height={16}
+								alt='category'
+							/>
+							카테고리
+						</div>
 					</div>
 					<Input
 						prefix={
@@ -36,22 +39,8 @@ function Toolbar() {
 						placeholder='살까말까 고민된다면 검색해보세요!'
 						containerStyle='hidden md:flex'
 					/>
-					<div className='flex flex-row gap-3 mr-5 md:hidden'>
-						<Image
-							src='https://www.testvalley.kr/common/bell_default.svg'
-							width={24}
-							height={24}
-							alt='bell'
-						/>
-						<Image
-							src='https://www.testvalley.kr/common/search_new.svg'
-							width={18}
-							height={20}
-							alt='search'
-						/>
-					</div>
 				</div>
-				<div className='hidden md:flex items-center justify-center'>
+				<div className='hidden md:flex md:flex-row items-center justify-center'>
 					<button className='mr-[8px]'>
 						<Image
 							src='https://www.testvalley.kr/common/home-event.svg'
@@ -69,9 +58,24 @@ function Toolbar() {
 						alt='vertical-bar'
 						className='mr-[12px]'
 					/>
-					<button className='text-center text-gray-700 text-sm'>
+					<div className='text-center text-gray-700 text-sm whitespace-nowrap'>
 						로그인 / 회원가입
-					</button>
+					</div>
+				</div>
+
+				<div className='flex flex-row gap-6 items-center mr-8 md:hidden'>
+					<Image
+						src='https://www.testvalley.kr/common/bell_default.svg'
+						width={24}
+						height={24}
+						alt='bell'
+					/>
+					<Image
+						src='https://www.testvalley.kr/common/search_new.svg'
+						width={18}
+						height={20}
+						alt='search'
+					/>
 				</div>
 			</div>
 		</div>
